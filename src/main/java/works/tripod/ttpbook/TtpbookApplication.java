ee5a5b27ee6979debde9ed12a03cad659dce2f19
sdpark.home <parksangdonews@gmail.com>
2021-04-18 07:26:03 +0900

    init: TTP Books 관리 및 정보제공 웹페이지 프로젝트 시작
    210418 sdpark
    하 ~~~~~~~~ 일단 시작.

---
+++ src/main/java/works/tripod/ttpbook/TtpbookApplication.java
@@ -0,0 +1,13 @@
+package works.tripod.ttpbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtpbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtpbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/works/tripod/ttpbook/TtpbookApplicationTests.java
@@ -0,0 +1,13 @@
+package works.tripod.ttpbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtpbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
