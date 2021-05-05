6c8a04571708fe9cb836b2d6d94932b7121c87f5
sdpark.home <parksangdonews@gmail.com>
2021-05-05 11:37:59 +0900

    docker error status

--- src/main/java/works/tripod/ttpbook/config/AppConfig.java
+++ src/main/java/works/tripod/ttpbook/config/AppConfig.java
@@ -2,6 +2,7 @@ package works.tripod.ttpbook.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
